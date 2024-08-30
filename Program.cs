8437d5e2fcf4af4db99d4fc20918c51593b0c777
nathaniel-creator <2493349@cstjean.qc.ca>
2024-08-30 10:49:54 -0400

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+const double ORDINATEUR_CONSOMMATION = 150;
+const double COUTS_WATTS = 0.15 / 100;
+
+double heures;
+double watts;
+double argent;
+
+Console.WriteLine("Veuillez rentrez le nombre d'heure");
+
+heures = Convert.ToDouble(Console.Readline());
+
+watts = heures * ORDINATEUR_CONSOMMATION;
+argent = nombrewatts * COUTS_WATTS;
+
+Console.Writeline($"coût total:{argent}")
+
+
+
+
+
